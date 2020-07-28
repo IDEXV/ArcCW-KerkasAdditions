1f2ffb9248746a132080a21a396ff2f0144ae36e
Maxim IDEXV <39655269+IDEXV@users.noreply.github.com>
2020-07-28 13:20:43 +0300

    two new scopes

---
+++ arccw-kerk/lua/arccw/shared/attachments/optic_eomptsmg.lua
@@ -0,0 +1,45 @@
+att.PrintName = "KOMPS"
+att.Icon = Material("entities/acwatt_optic_eompts.png")
+att.Description = "Light prototype scope. KOMPS stands for 'KerkOTech Miniature Prototype Sight'."
+
+att.SortOrder = 40
+
+att.Desc_Pros = {
+    "+ Precision sight picture",
+}
+att.Desc_Cons = {
+}
+att.AutoStats = true
+att.Slot = "optic"
+
+att.Model 				= "models/weapons/arccw/atts/eompts_4b.mdl"
+att.HolosightPiece 	= "models/weapons/arccw/atts/eompts_hsp_4a.mdl"
+
+att.AdditionalSights = {
+    {
+        Pos = Vector(-0.01, 12, -1.52),
+        Ang = Angle(0, 0, 0),
+        Magnification = 1.75,
+        ScrollFunc = ArcCW.SCROLL_ZOOM,
+        ZoomSound = "buttons/combine_button5.wav",
+        IgnoreExtra = true,
+    }
+}
+
+att.ScopeGlint = false
+
+att.Holosight = true
+att.HolosightReticle = Material("hud/scopes/eompts.png")
+att.Colorable = false
+att.HolosightNoFlare = true
+att.HolosightSize = 24
+att.HolosightBone = "holosight"
+
+att.HolosightMagnification = 1.1
+att.HolosightBlackbox = false
+
+att.HolosightConstDist = 42
+
+att.Mult_SightTime = 1.2
+att.Mult_SightedSpeedMult = 0.85
+att.Mult_SpeedMult = 0.865
---
+++ arccw-kerk/lua/arccw/shared/attachments/optic_eomptsnv.lua
@@ -0,0 +1,50 @@
+att.PrintName = "KOMPNVS (NV)"
+att.Icon = Material("entities/acwatt_optic_eompts.png")
+att.Description = "Light night vision scope. KOMPNVS stands for 'KerkOTech Miniature Prototype Night Vision Sight'. Quite heavy."
+
+att.SortOrder = 40
+
+att.Desc_Pros = {
+    "+ Precision sight picture",
+    "+ Night vision picture"
+}
+att.Desc_Cons = {
+    "- Visible scope glint",
+}
+att.AutoStats = true
+att.Slot = "optic"
+
+att.Model 				= "models/weapons/arccw/atts/eompts_4b.mdl"
+att.HolosightPiece 	= "models/weapons/arccw/atts/eompts_hsp_4a.mdl"
+
+att.AdditionalSights = {
+    {
+        Pos = Vector(-0.01, 12, -1.52),
+        Ang = Angle(0, 0, 0),
+        Magnification = 1.75,
+        ScrollFunc = ArcCW.SCROLL_ZOOM,
+        NVScope = true,
+        NVScopeColor = Color(0, 255, 100),
+        NVFullColor = true,
+        ZoomSound = "buttons/combine_button5.wav",
+        IgnoreExtra = true,
+    }
+}
+
+att.ScopeGlint = true
+
+att.Holosight = true
+att.HolosightReticle = Material("hud/scopes/eompts.png")
+att.Colorable = false
+att.HolosightNoFlare = true
+att.HolosightSize = 24
+att.HolosightBone = "holosight"
+
+att.HolosightMagnification = 1.1
+att.HolosightBlackbox = false
+
+att.HolosightConstDist = 42
+
+att.Mult_SightTime = 1.5
+att.Mult_SightedSpeedMult = 0.80
+att.Mult_SpeedMult = 0.885
