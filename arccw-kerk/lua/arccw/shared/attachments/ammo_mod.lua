c1b9b09d531bdf5293cc9f521db6ccdc7be23323
Maxim IDEXV <39655269+IDEXV@users.noreply.github.com>
2020-07-28 22:37:26 +0300

    new shotgun/rifle ammo

---
+++ arccw-kerk/lua/arccw/shared/attachments/ammo_mod.lua
@@ -0,0 +1,13 @@
+att.PrintName = "KerPX-20"
+att.Icon = Material("entities/acwatt_ammo_tmj.png")
+att.Description = "Special .50 caliber bullets which keep energy better at long range, improving damage at distance but overpenetrating targets which are too close."
+att.Desc_Pros = {
+}
+att.Desc_Cons = {
+}
+att.AutoStats = true
+att.Slot = "ammo_bullet"
+
+att.Mult_Damage = 0.4
+att.Mult_DamageMin = 3
+att.Mult_Penetration = 5
