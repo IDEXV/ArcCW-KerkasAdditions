eb6e7f8a714f73b3fc1b087104adefe16bf80bef
Maxim IDEXV <39655269+IDEXV@users.noreply.github.com>
2020-07-28 12:10:05 +0300

    fixed #2

--- arccw-kerk/lua/arccw/shared/attachments/perk_cmags.lua
+++ arccw-kerk/lua/arccw/shared/attachments/perk_cmags.lua
@@ -1,8 +1,8 @@
 att.PrintName = "C-Mags"
 att.Icon = Material("entities/acwatt_perk_extendedmags.png")
-att.Description = "High-capacity, C-Shaped magazines allow for more time before needing to reload, but slow down reloading and add increase weapon recoil."
+att.Description = "High-capacity, C-Shaped magazines allow for even more time before needing to reload, but slow down reloading and increase weapon recoil."
 att.Desc_Pros = {
-    "+ More ammo",
+    "++ More ammo",
 }
 att.Desc_Cons = {
 }
@@ -11,14 +11,17 @@ att.Slot = "ammo_bullet"
 
 att.Mult_ReloadTime = 1.40
 att.MagExtender = true
-att.Add_ChamberSize = 30
 
 att.ActivateElements = {"extendedmag"}
 
-att.Mult_SightTime = 1.25
+att.Mult_SightTime = 1.35
 
 att.Mult_HipDispersion = 1.35
-att.Mult_Recoil = 1.25
+att.Mult_Recoil = 1.30
+
+att.Hook_GetCapacity = function(wep, cap)
+    return 100
+end
 
 att.Hook_Compatible = function(wep)
     if (wep.RegularClipSize or wep.Primary.ClipSize) == wep.ExtendedClipSize then return false end
