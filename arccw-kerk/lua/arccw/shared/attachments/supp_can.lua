d5c2c6a0e7a94e48342364b280945811ecbf715d
Maxim IDEXV <39655269+IDEXV@users.noreply.github.com>
2020-07-29 16:29:10 +0300

    new suppressors

---
+++ arccw-kerk/lua/arccw/shared/attachments/supp_can.lua
@@ -0,0 +1,26 @@
+att.PrintName = "Can Suppressor"
+att.Icon = Material("entities/acwatt_supp_heavy.png")
+att.Description = "An improvised suppressor, made out of a tin can. Somewhat makes the weapon quieter, but the suppressor is quite heavy."
+att.Desc_Pros = {
+}
+att.Desc_Cons = {
+    "- May obstruct sights"
+}
+att.AutoStats = true
+att.Slot = {"muzzle", "muzzle_shotgun"}
+
+att.Model = "models/weapons/arccw/atts/supp_heavy.mdl"
+
+att.Silencer = true
+att.Override_MuzzleEffect = "muzzleflash_suppressed"
+att.IsMuzzleDevice = true
+
+att.Mult_ShootPitch = 0.9
+att.Mult_ShootVol = 0.97
+att.Mult_AccuracyMOA = 0.90
+att.Mult_Range = 1.10
+
+att.Mult_SightTime = 1.2
+att.Mult_DrawTime = 1.2
+
+att.Add_BarrelLength = 24
---
+++ arccw-kerk/lua/arccw/shared/attachments/supp_distancer.lua
@@ -0,0 +1,27 @@
+att.PrintName = "Distancer"
+att.Icon = Material("entities/acwatt_supp_med.png")
+att.Description = "A heavily modified suppressor, which makes the bullets travel further. Moderately improves performance, but increases weapon recoil."
+att.Desc_Pros = {
+}
+att.Desc_Cons = {
+}
+att.AutoStats = true
+att.Slot = "muzzle"
+
+att.Model = "models/weapons/arccw/atts/supp_medium.mdl"
+
+att.Silencer = true
+att.Override_MuzzleEffect = "muzzleflash_suppressed"
+att.IsMuzzleDevice = true
+
+att.Mult_ShootVol = 0.90
+att.Mult_AccuracyMOA = 0.8
+att.Mult_Range = 1.20
+att.Mult_DamageMin = 1.15
+att.Mult_MuzzleVelocity = 1.05
+
+att.Mult_Recoil = 1.35
+att.Mult_RecoilSide = 1.35
+att.Mult_HipDispersion = 1.35
+
+att.Add_BarrelLength = 14
