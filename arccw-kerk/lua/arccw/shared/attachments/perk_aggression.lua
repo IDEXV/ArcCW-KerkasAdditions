17560cf5decb67a02d43c068997330eb64b88a00
Maxim IDEXV <39655269+IDEXV@users.noreply.github.com>
2020-08-02 22:12:37 +0300

    balancing fixes

--- arccw-kerk/lua/arccw/shared/attachments/perk_aggression.lua
+++ arccw-kerk/lua/arccw/shared/attachments/perk_aggression.lua
@@ -1,6 +1,6 @@
 att.PrintName = "Aggression"
 att.Icon = Material("entities/acwatt_perk_streetjustice.png", "smooth mips")
-att.Description = "Concentrate all your fury into every bash for increased melee damage. Your fury allows for faster reloading. The downside is bad recoil control."
+att.Description = "Concentrate all your fury into every bash for increased melee damage. Your fury allows for faster reloading. The downside is bad recoil control and weapon accuracy."
 att.Desc_Pros = {
 }
 att.Desc_Cons = {
@@ -15,8 +15,11 @@ att.Mult_MeleeTime = 1.35
 att.Mult_DrawTime = 0.9
 att.Mult_ReloadTime = 0.6
 
+att.Mult_AccuracyMOA = 1.30
+att.Mult_HipDispersion = 1.30
+att.Mult_SightsDispersion = 1.30
 att.Mult_Recoil = 1.35
 att.Mult_RecoilSide = 1.35
 att.Mult_VisualRecoilMult = 1.35
-att.Mult_SightTime = 1.20
-att.Mult_CycleTime = 1.25
+att.Mult_SightTime = 1.30
+att.Mult_CycleTime = 1.30
