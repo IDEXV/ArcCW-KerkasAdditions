7e6ab4c18005aa45859a9482bd04d687a85cab81
Maxim IDEXV <39655269+IDEXV@users.noreply.github.com>
2020-08-05 20:48:14 +0300

    new perk

---
+++ arccw-kerk/lua/arccw/shared/attachments/perk_steadyhands.lua
@@ -0,0 +1,17 @@
+att.PrintName = "Steady Hands"
+att.Icon = Material("entities/acwatt_perk_lightweight.png")
+att.Description = "Buffed arms allow for decreased weapon recoil. Your only problem is bad accuracy and hip fire spread."
+att.Desc_Pros = {
+}
+att.Desc_Cons = {
+}
+att.AutoStats = true
+att.Slot = "perk"
+
+att.Mult_Recoil = 0.7
+att.Mult_MeleeDamage = 1.20
+
+att.Mult_AccuracyMOA = 1.30
+att.Mult_HipDispersion = 1.30
+att.Mult_SightsDispersion = 1.30
+att.Mult_RecoilSide = 1.30
