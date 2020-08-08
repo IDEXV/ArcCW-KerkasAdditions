1742f1f87322679ff78adcbbba7a264019326884
Maxim IDEXV <39655269+IDEXV@users.noreply.github.com>
2020-08-08 12:14:59 +0300

    non-lethal ammo

---
+++ arccw-kerk/lua/arccw/shared/attachments/ammo_nonlethal.lua
@@ -0,0 +1,29 @@
+att.PrintName = "Non-Lethal Ammo"
+att.Icon = Material("entities/ammo_mod.png")
+att.Description = "Special rubber bullets used for riot control. These bullets are mainly non-lethal, but overuse can cause severe blunt trauma. Perfect when you want to stop somebody from escaping."
+att.Desc_Pros = {
+}
+att.Desc_Cons = {
+    "Non-lethal bullets",
+}
+att.AutoStats = true
+att.Slot = "ammo_bullet"
+
+att.Mult_Damage = 0.2
+att.Mult_DamageMin = 0.2
+att.Mult_Recoil = 0.7
+att.Mult_AccuracyMOA = 0.7
+att.Mult_HipDispersion = 0.7
+att.Mult_SightsDispersion = 0.7
+att.Mult_SpeedMult = 0.85
+att.Mult_SightedSpeedMult = 0.85
+att.Mult_ReloadTime = 1.25
+att.Mult_CycleTime = 1.25
+att.Mult_ShootPitch = 1.15
+
+att.Override_ShootEntity = "arccw_gl_training"
+att.Mult_MuzzleVelocity = 40
+
+att.MagReducer = true
+
+att.ActivateElements = {"reducedmag"}
