54fc3fd8b27f2c976945b318aff4155a22cc0450
Maxim IDEXV <39655269+IDEXV@users.noreply.github.com>
2020-08-11 23:03:00 +0300

    suppressor device nerfs

--- arccw-kerk/lua/arccw/shared/attachments/supp_tech.lua
+++ arccw-kerk/lua/arccw/shared/attachments/supp_tech.lua
@@ -1,6 +1,6 @@
 att.PrintName = "Suppressor Device"
 att.Icon = Material("entities/acwatt_supp_tech.png")
-att.Description = "A balanced sound suppressor, with a strange device attached. Moderately improves performance, and decreases weapon recoil."
+att.Description = "A balanced sound suppressor, with a strange device attached. Moderately improves performance, and decreases weapon recoil. Made by KerkOTech."
 att.Desc_Pros = {
 }
 att.Desc_Cons = {
@@ -25,21 +25,21 @@ att.Override_Firemodes = {
     }
 }
 
-att.Mult_ShootVol = 0.55
+att.Mult_ShootVol = 0.85
 att.Mult_AccuracyMOA = 0.7
 att.Mult_Range = 1.25
 att.Mult_Recoil = 0.9
 att.Mult_RecoilSide = 0.9
-att.Mult_VisualRecoilMult = 1
-att.Mult_Damage = 1.15
-att.Mult_DamageMin = 1.05
+att.Mult_VisualRecoilMult = 1.15
+att.Mult_Damage = 1.20
+att.Mult_DamageMin = 1.10
 att.Mult_MuzzleVelocity = 1.35
 
-att.Mult_SightTime = 1.35
-att.Mult_HipDispersion = 1.35
+att.Mult_SightTime = 1.40
+att.Mult_HipDispersion = 1.40
 att.Mult_ReloadTime = 1.45
-att.Mult_DrawTime = 1.35
-att.Mult_CycleTime = 1.35
+att.Mult_DrawTime = 1.40
+att.Mult_CycleTime = 1.40
 att.Mult_RPM = 0.45
 
 att.Add_BarrelLength = 14
