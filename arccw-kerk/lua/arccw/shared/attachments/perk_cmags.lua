ae41fa79740dbfeb36cefae6fef44d4024bf660d
Maxim IDEXV <39655269+IDEXV@users.noreply.github.com>
2020-08-12 12:55:34 +0300

    improved the c-mags

--- arccw-kerk/lua/arccw/shared/attachments/perk_cmags.lua
+++ arccw-kerk/lua/arccw/shared/attachments/perk_cmags.lua
@@ -11,29 +11,29 @@ att.Desc_Neutrals = {
 att.AutoStats = true
 att.Slot = "ammo_bullet"
 
-att.Mult_ReloadTime = 1.40
-att.MagExtender = true
+att.Mult_ReloadTime = 1.45
 
 att.ActivateElements = {"extendedmag"}
 
-att.Mult_SightTime = 1.35
-att.Mult_DrawTime = 1.35
+att.Mult_SightTime = 1.45
+att.Mult_DrawTime = 1.45
 att.Mult_MeleeDamage = 1.25
-att.Mult_MeleeAttackTime = 1.25
+att.Mult_MeleeAttackTime = 1.30
 
 att.Mult_HipDispersion = 1.35
-att.Mult_SightsDispersion = 1.25
-att.Mult_AccuracyMOA = 1.15
-att.Mult_RecoilSide = 1.15
-att.Mult_VisualRecoilMult = 1.25
-att.Mult_Recoil = 1.25
-att.Mult_SpeedMult = 0.85
-att.Mult_SightedSpeedMult = 0.85
+att.Mult_SightsDispersion = 1.35
+att.Mult_AccuracyMOA = 1.35
+att.Mult_Recoil = 1.35
+att.Mult_RecoilSide = 1.35
+att.Mult_VisualRecoilMult = 1.35
+att.Mult_SpeedMult = 0.65
+att.Mult_SightedSpeedMult = 0.65
+att.Mult_CycleTime = 1.35
 
 att.Hook_Compatible = function(wep)
     if wep.Num ~= 1 or (wep.Primary.Ammo ~= "smg1" and wep.Primary.Ammo ~= "ar2") then return false end
 end
 
 att.Hook_GetCapacity = function(wep, cap)
-    return 100
+    return 150
 end
