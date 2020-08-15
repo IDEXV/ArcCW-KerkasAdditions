52b2d81d7122c3e61bb80a7788b281d516c84f2f
Maxim IDEXV <39655269+IDEXV@users.noreply.github.com>
2020-08-15 12:46:29 +0300

    improved doom style

--- arccw-kerk/lua/arccw/shared/attachments/pistol_doom.lua
+++ arccw-kerk/lua/arccw/shared/attachments/pistol_doom.lua
@@ -1,10 +1,13 @@
 att.PrintName = "Doom Style"
 att.Icon = Material("entities/acwatt_pistol_doom.png")
-att.Description = "An ancient style of holding a gun. It's seems oddly familiar."
+att.Description = "An ancient style of holding a weapon. It's seems oddly familiar."
 att.Desc_Pros = {
     "Shoot while sprinting"
 }
 att.Desc_Cons = {
+}
+att.Desc_Neutrals = {
+    "Might be broken on some weapons",
 }
 att.Slot = {"style_pistol", "foregrip"}
 att.Free = true
@@ -18,6 +21,6 @@ att.Mult_SightedSpeedMult = 0.2
 att.Mult_SightTime = 6
 att.Override_HoldtypeActive = "shotgun"
 
-att.Override_ActivePos = Vector(-6, 0, 0)
+att.Override_ActivePos = Vector(-5, 0, 0)
 
 att.Override_ShootWhileSprint = true
