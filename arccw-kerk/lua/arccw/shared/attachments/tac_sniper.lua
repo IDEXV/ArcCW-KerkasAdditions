ab6a7ac1c36e71d1ce2e5f4eb47c78f885ada303
Maxim IDEXV <39655269+IDEXV@users.noreply.github.com>
2020-08-20 22:59:42 +0300

    new laser sight

---
+++ arccw-kerk/lua/arccw/shared/attachments/tac_sniper.lua
@@ -0,0 +1,31 @@
+att.PrintName = "SNPX-20 Laser"
+att.Icon = Material("entities/acwatt_tac_anpeq.png")
+att.Description = "A very powerful red laser aiming module. This very bright laser assists with aiming. Far more effective than the Red Laser, but can only be used on sniper rifles. Made by KerkOTech."
+att.Desc_Pros = {
+}
+att.Desc_Cons = {
+    "con.beam"
+}
+att.Desc_Neutrals = {
+    "Can only be used with sniper ammo",
+}
+att.AutoStats = true
+att.Slot = "tac"
+
+att.Model = "models/weapons/arccw/atts/laser_anpeq.mdl"
+
+att.Laser = true
+att.LaserStrength = 5
+att.LaserBone = "laser"
+
+att.ColorOptionsTable = {Color(255, 50, 50)}
+
+att.Mult_SightTime = 1.5
+
+att.Mult_HipDispersion = 0.3
+att.Mult_AccuracyMOA = 0.3
+att.Mult_SightsDispersion = 0.3
+
+att.Hook_Compatible = function(wep)
+    if wep.Num ~= 1 or (wep.Primary.Ammo ~= "SniperPenetratedRound") then return false end
+end
