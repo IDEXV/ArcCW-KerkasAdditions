60c3ac8d6940c1aba7171d65bfbd551de023ac40
Maxim IDEXV <39655269+IDEXV@users.noreply.github.com>
2020-08-28 19:33:17 +0300

    style improvements

--- arccw-kerk/lua/arccw/shared/attachments/pistol_hipnew.lua
+++ arccw-kerk/lua/arccw/shared/attachments/pistol_hipnew.lua
@@ -2,7 +2,7 @@ att.PrintName = "Hip Style"
 att.Icon = Material("entities/acwatt_pistol_onehand.png")
 att.Description = "A shooting style that lowers the gun and uses one's sheer body mass to absorb recoil. Amazing control at the cost of precision and slow aiming."
 att.Desc_Pros = {
-    "+ Incredible recoil control",
+    "Incredible recoil control",
 }
 att.Desc_Cons = {
 }
@@ -17,8 +17,8 @@ att.Mult_Recoil = 0.4
 att.Mult_RecoilSide = 0.4
 att.Mult_RecoilRise = 0.5
 
-att.Mult_SightTime = 4
-att.Mult_PrecisionMOA = 0.8
+att.Mult_SightTime = 8
+att.Mult_PrecisionMOA = 0.7
 att.Mult_SightedMoveSpeed = 0.2
 att.Override_HoldtypeActive = "slam"
 
