74cbd445c126f6a2475df8b105c97609713f2ede
Maxim IDEXV <39655269+IDEXV@users.noreply.github.com>
2020-09-09 13:30:43 +0300

    bugfix

--- arccw-kerk/lua/arccw/shared/attachments/supp_unknown.lua
+++ arccw-kerk/lua/arccw/shared/attachments/supp_unknown.lua
@@ -1,11 +1,10 @@
 att.PrintName = "Prototype Suppressor"
 att.Icon = Material("entities/acwatt_supp_prototype.png")
-att.Description = "Large prototype sound suppressor with insane suppressing qualities. Many suppressor modifications make the weapon very bulky, and due to these modifications, overuse will break the suppressor. Made by KerkOTech."
+att.Description = "A large prototype sound suppressor with insane suppressing qualities. Many suppressor modifications decrease weapon efficiency. Made by KerkOTech."
 att.Desc_Pros = {
 }
 att.Desc_Cons = {
     "May obstruct sights",
-    "Fragile suppressor, will break after 20 shots",
 }
 att.AutoStats = true
 att.Slot = {"muzzle", "muzzle_shotgun"}
@@ -17,7 +16,7 @@ att.Override_MuzzleEffect = "muzzleflash_suppressed"
 att.IsMuzzleDevice = true
 
 att.Mult_ShootPitch = 0.4
-att.Mult_ShootVol = 0.25
+att.Mult_ShootVol = 0.30
 att.Mult_Range = 1.25
 
 att.Mult_SightTime = 1.8
@@ -33,10 +32,3 @@ att.Mult_SpeedMult = 0.65
 att.Mult_SightedSpeedMult = 0.65
 
 att.Add_BarrelLength = 32
-
-att.DamageOnShoot = 1
-att.Health = 20
-
-att.Hook_AttDestroyed = function(wep, data)
-    wep:EmitSound("weapons/arccw/supp_break.wav", 75, 100, 1, CHAN_STATIC)
-end
